5b8385b3a0aae15a944c7f5dbb3a92752e04a089
Loma <lomakim@gmail.com>
2026-04-24 21:47:06 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -20,30 +20,37 @@ int main() {
         tBooth.push_back(*temp);
         delete temp;
     }
+
+    // Print initial Queue
     cout << "Initial Queue: " << endl;
     printLine(tBooth);
+    cout << endl;
 
+    // Do-while loop randomly adds or removes car from queue and prints outcome
     do {
-        cout << "Time: " << opNum << "Operation: "; 
+        cout << "Time: " << opNum << " Operation: "; 
         probability = (rand() % 100) + 1;
+
         // 55% probability the first car pays and leaves the line;
         if (probability <= 55) {
             t = tBooth[0];
             cout << "Car paid: ";
             t.print();
-            cout << endl;
             tBooth.pop_front();
         }
+        // 45% probability a car joins the line for the toll booth
         else {
             Car* temp = new Car;
             t = *temp;
+            delete temp;
             cout << "Joined lane: ";
             t.print();
-            cout << endl;
             tBooth.push_back(t);
-
-
         }
+        opNum++;                    // Increment operation counter
+        cout << "Queue: " << endl;
+        printLine(tBooth);
+        cout << endl;
 
     } while (tBooth.empty() == false);
 
@@ -52,9 +59,13 @@ int main() {
 
 void printLine(deque<Car> t) {
     Car element;
-    for(int i = 0; i < t.size(); i++){
-            element = t[i];
-            cout << "\t";
-            element.print();
+    if (t.empty() == true)
+        cout << "\tEmpty" << endl;
+    else {
+        for(int i = 0; i < t.size(); i++){
+                element = t[i];
+                cout << "\t";
+                element.print();
+        }
     }
 }
