41446e6ecbdbde76eb9970be81c049f659aebde3
Loma <lomakim@gmail.com>
2026-04-24 21:04:27 -0700

    Update

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// COMSC-210 | Lab 32 | Loma Kim
+#include <iostream>
+#include "car.h"
+#include <deque>
+using namespace std;
+deque<Car> tBooth;
+
+int main() {
+    const int SIZE = 2;
+
+    Car t1;
+    tBooth.push_back(t1);
+    Car t2;
+    tBooth.push_back(t2);
+
+    return 0;
+}
