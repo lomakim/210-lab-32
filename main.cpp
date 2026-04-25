3de7a09635471dbfe2c7d9a5559c8dc2637d124c
Loma <lomakim@gmail.com>
2026-04-24 21:13:18 -0700

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -6,12 +6,19 @@ using namespace std;
 deque<Car> tBooth;
 
 int main() {
+    srand(time(0));
     const int SIZE = 2;
+    Car element;
 
-    Car t1;
+    // Cars initially in the toll booth line
+    Car t1, t2;
     tBooth.push_back(t1);
-    Car t2;
     tBooth.push_back(t2);
 
+    for(int i = 0; i < tBooth.size(); i++){
+        element = tBooth[i];
+        element.print();
+    }
+    
     return 0;
 }
