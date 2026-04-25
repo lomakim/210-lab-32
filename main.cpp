4410352b4cdcc3da019abb680810260ffe62b7d1
Loma <lomakim@gmail.com>
2026-04-24 21:25:08 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -5,20 +5,36 @@
 using namespace std;
 deque<Car> tBooth;
 
+void printLine(deque<Car>);
+
 int main() {
     srand(time(0));
     const int SIZE = 2;
-    Car element;
+    int probability;
 
     // Cars initially in the toll booth line
-    Car t1, t2;
-    tBooth.push_back(t1);
-    tBooth.push_back(t2);
-
-    for(int i = 0; i < tBooth.size(); i++){
-        element = tBooth[i];
-        element.print();
+    for (int i = 0; i < SIZE; i++) {
+        Car* temp = new Car;
+        tBooth.push_back(*temp);
     }
-    
+
+
+    printLine(tBooth);
+
+    do {
+        probability = (rand() % 100) + 1;
+        if (probability <= 55) {
+
+        }
+    } while (tBooth.empty() == false);
+
     return 0;
+}
+
+void printLine(deque<Car> t) {
+    Car element;
+    for(int i = 0; i < t.size(); i++){
+            element = t[i];
+            element.print();
+    }
 }
